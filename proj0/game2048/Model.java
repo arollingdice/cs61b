1e350a2895b13d50ad00efcc3c381822b822d97b
arollingdice <arollingdice@gmail.com>
2023-05-18 23:12:47 -0600

    finished emptySpaceExists() and maxTileExists() in Model.java

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -5,7 +5,7 @@ import java.util.Observable;
 
 
 /** The state of a game of 2048.
- *  @author TODO: YOUR NAME HERE
+ *  @author Yucong Mo
  */
 public class Model extends Observable {
     /** Current contents of the board. */
@@ -137,7 +137,12 @@ public class Model extends Observable {
      *  Empty spaces are stored as null.
      * */
     public static boolean emptySpaceExists(Board b) {
-        // TODO: Fill in this function.
+        int s = b.size();
+        for (int i = 0; i < s; i ++) {
+            for (int j = 0; j < s; j ++) {
+                if(b.tile(i, j) == null) return true;
+            }
+        }
         return false;
     }
 
@@ -147,7 +152,13 @@ public class Model extends Observable {
      * given a Tile object t, we get its value with t.value().
      */
     public static boolean maxTileExists(Board b) {
-        // TODO: Fill in this function.
+        int s = b.size();
+        for (int i = 0; i < s; i ++) {
+            for (int j = 0; j < s; j ++) {
+                if(b.tile(i, j) == null) continue;
+                if(b.tile(i, j).value() == MAX_PIECE) return true;
+            }
+        }
         return false;
     }
 
